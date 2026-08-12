516457ef1389411b8dda41305d71d2f26950866d
Ashish Kumar Yadav <ashishkumar12333ak@gmail.com>
2026-08-12 14:56:08 +0530

    Time: 0 ms (100%), Space: 43.8 MB (16.05%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int sum=0;
+        for(int i:nums) sum+=i;
+        if(sum%2==0) return nums.length-1;
+        return 0;
+    }
+}
